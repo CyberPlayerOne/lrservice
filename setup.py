18b0ab905cb452a8b59a147d2fdeb19e18eaccce
tyler <tyler.x.tang@outlook.com>
2023-06-01 17:45:14 +1000

    fixed dependencies in packaging with setuptools

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = "A python package for demonstrating how to make REST API serv
 
 setuptools.setup(
     name="lrservice",
-    version="0.0.4",
+    version="0.0.5",
     author="Tyler Tang",
     author_email="tyler.x.tang@outlook.com",
     description="A python package for demonstrating how to make REST API service for linear regression",
@@ -14,6 +14,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/CyberPlayerOne/lrservice",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'scikit-learn==1.2.2',
+        'matplotlib==3.7.1',
+        'numpy==1.24.3',
+        'flask==2.2.2',
+        'gunicorn==20.1.0',
+        'dill==0.3.6'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
